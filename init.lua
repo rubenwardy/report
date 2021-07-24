8b2ff073ea876e795a2ec40037c9b9edb16f15b4
sys4 <bricassa@sys4.fr>
2021-07-25 00:01:37 +0200

    Can send reports to offline moderators + add french translation

--- init.lua
+++ init.lua
@@ -1,55 +1,99 @@
-local function save_modo(name)
+local S = minetest.get_translator("report")
 
-end
+local storage = minetest.get_mod_storage()
+assert(storage, "mod_storage is required")
 
 local function get_modo_list()
+	local store = storage:get_string("report")
+	if store then
+		return minetest.deserialize(store) or {}
+	end
 
+	return nil
+end
+
+local function is_known_modo(name)
+	for _, modo in ipairs(get_modo_list()) do
+		if modo == name then
+			return true
+		end
+	end
+	return false
 end
 
 minetest.register_on_joinplayer(function(player)
-		-- 1. Obtenir la liste des modérateur connus
-		--    local modo_saved = get_modo_list()
-		-- 2. Obtenir les privs du player
-		-- 3. Si player n'est pas dans la liste modos connus et a privs modos alors
-		-- 4.  Ajouter nom du player dans liste modos connus
-		-- 5. Sinon si player est dans liste modos connus et n'a pas privs modos alors
-		-- 6.  Supprimer nom du player de la liste modos connus.
-		-- 7. Fin
+	-- Obtenir les privs du player
+	local player_name = player:get_player_name()
+	local is_modo = minetest.check_player_privs(player_name, {kick = true, ban = true})
+
+	-- Le joueur est-il un modérateur connu ?
+	local known = is_known_modo(player_name)
+
+	-- Ajouter ou supprimer le joueur de la liste des modérateurs
+	if not known and is_modo then
+		-- Ajouter nom du player dans liste modos
+		local modos = get_modo_list()
+		table.insert(modos, player_name)
+		storage:set_string("report", minetest.serialize(modos))
+	elseif known and not is_modo then
+		-- Supprimer nom du player de la liste modos
+		local modos = get_modo_list()
+		for i, modo in ipairs(modos) do
+			if modo == player_name then
+				table.remove(modos, i)
+				storage:set_string("report", minetest.serialize(modos))
+				break
+			end
+		end
+	end
 end)
 
 minetest.register_chatcommand("report", {
+	description = S("Report: @1 to see moderators list. @2 is the message to report to moderators.", "'modos'", "'msg'"),
+	params = "modos|msg",
 	func = function(name, param)
 		param = param:trim()
 		if param == "" then
-			return false, "Veuillez écrire un message pour le rapport. " ..
-				"Si il s'agit d'un (de) joueur(s) en particulier, veuillez aussi indiquer son (leur) nom(s)."
+			return false, S("Please write a message for the report. If it's about one or several players in particular, please give their name.")
+		end
+		if param == "modos" then
+			return true, S("Moderators list: @1", table.concat(get_modo_list(), ", "))
 		end
 		local _, count = string.gsub(param, " ", "")
 		if count == 0 then
-			minetest.chat_send_player(name, "Si vous rapportez l'attitude d'un joueur, " ..
-				"vous devriez aussi dire quelle en est la raison. (Ex: insulte, sabotage)")
+			minetest.chat_send_player(name, S("If you are reporting a player's attitude, you should also say why. (Ex: insult, sabotage)"))
 		end
 
 		-- Send to online moderators / admins
 		-- Get comma separated list of online moderators and admins
-		local mods = {}
+		local modos_online = {}
 		for _, player in pairs(minetest.get_connected_players()) do
 			local toname = player:get_player_name()
-			if minetest.check_player_privs(toname, {kick = true, ban = true}) then
-				table.insert(mods, toname)
-				minetest.chat_send_player(toname, "-!- " .. name .. " a rapporté : " .. param)
+			if is_known_modo(toname) then
+				table.insert(modos_online, toname)
+				minetest.chat_send_player(toname, S("-!- @1 has reported: @2", name, param))
 			end
 		end
 
-		if #mods > 0 then
-			mod_list = table.concat(mods, ", ")
-			email.send_mail(name, minetest.setting_get("name"),
-								 "Rapport: " .. param .. " (modos connecté(s) : " .. mod_list .. ")")
-			return true, "Rapporté. Moderateur(s) connecté(s) : " .. mod_list
+		local admin = minetest.setting_get("name")
+
+		if #modos_online > 0 then
+			local mod_list = table.concat(modos_online, ", ")
+			email.send_mail(name, admin, S("Report: @1 (online moderator(s): @2)", param, mod_list))
+			for _, modo in ipairs(get_modo_list()) do
+				if modo ~= admin then
+					email.send_mail(name, modo, S("Report: @1 (online moderator(s): @2)", param, mod_list))
+				end
+			end
+			return true, S("Reported. Online moderator(s): @1", mod_list)
 		else
-			email.send_mail(name, minetest.setting_get("name"),
-				"Rapport: " .. param .. " (pas de modo connecté)")
-			return true, "Rapporté. On reviendra vers vous."
+			email.send_mail(name, admin, S("Report:  (no online moderator)", param))
+			for _, modo in ipairs(get_modo_list()) do
+				if modo ~= admin then
+					email.send_mail(name, modo, S("Report: @1 (no online moderator)", param))
+				end
+			end
+			return true, S("Reported. We will get back to you.")
 		end
 	end
 })
